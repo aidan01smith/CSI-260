81ad239953353f73dcbfac193881b9445a2e024f
aidan01smith <143528066+aidan01smith@users.noreply.github.com>
2025-01-23 14:33:22 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 '''
+This is a lab on list comprehensions
 1.    Generate a list of all of the numbers from 1-1000 that are divisible by 7
 2.    Generate a list of all of the numbers from 1-1000 that have the digit 3 in them
 3.    Remove all of the vowels in a string (can assume y is not a vowel)
@@ -13,6 +14,7 @@ Additional Challenges:
 
 # generate a list of all the numbers from 1-1000 that are divisible by 7
 
+'''
 new_list = []
 def divide_7():
     for i in range(1, 1001):
@@ -22,9 +24,17 @@ def divide_7():
     return new_list
 
 # print(divide_7())
+'''
+
+new_list = []
+def divide_7():
+    new_list = [i for i in range(1, 1001) if i % 7 == 0]
+    
+    return new_list
 
 # generate a list of all the numbers from 1-1000 that have the digit 3 in them
 
+'''
 list2 = []
 def digit3():
     for i in range(1,1001):
@@ -34,9 +44,17 @@ def digit3():
     return list2
 
 # print(digit3())
+'''
+
+list2 = []
+def digit3():
+    list2 = [i for i in range(1, 1001) if '3' in str(i)]
+    
+    return list2
 
 # remove all of the vowels in a string (assume y is not a vowel)
 
+'''
 vowels = ['a', 'e', 'i', 'o', 'u']
 def remove_vowel(string):
     new_string = ''
@@ -47,9 +65,18 @@ def remove_vowel(string):
     return new_string
 
 # print(remove_vowel('hello'))
+'''
+
+vowels = []
+def remove_vowel(string):
+    vowels = [i for i in string if i not in 'aeiou']
+    return ''.join(vowels)
+
+
 
 # find all of the words in a string that are less than 4 letters
 
+'''
 small_words = []
 
 def small_word(string):
@@ -61,9 +88,16 @@ def small_word(string):
 
 
 # print(small_word('hello my name is valiant and i am a student'))
+'''
+
+small_words = []
+def small_word(string):
+    small_words = [i for i in string.split() if len(i) < 4]
+    return small_words
 
 # use a dictionary comprehension that maps each word in a sentence to its length
 
+'''
 empty_dict = {}
 
 def length_word(string):
@@ -74,3 +108,9 @@ def length_word(string):
     return empty_dict
 
 # print(length_word('hello my name is valiant and i am a student'))
+'''
+
+empty_dict = {}
+def length_word(string):
+    empty_dict = {i: len(i) for i in string.split()}
+    return empty_dict
