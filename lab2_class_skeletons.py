9c5c2ae8da63fe05e5f93ce5d69a7e2ab9cb3c4c
aidan01smith <143528066+aidan01smith@users.noreply.github.com>
2025-01-30 15:27:11 -0500

    Update lab2_class_skeletons.py

--- lab2_class_skeletons.py
+++ lab2_class_skeletons.py
@@ -1,4 +1,4 @@
-class player():
+class Player:
     '''
     The player class that defines which player is who and what cards they have
     The player might also be replaced with a bot that plays the game itself
@@ -26,7 +26,7 @@ class player():
     def __str__(self):
         return f"{self.name} has {self.cards}"
     
-class deck():
+class Deck:
     # the deck class that defines the deck of cards
     def __init__(self, cards):
         self.cards = cards
@@ -37,7 +37,7 @@ class deck():
     def shuffle(self):
         pass
 
-class card():
+class Card:
 
     # this is the card class to that defines the cards in the deck
     def __init__(self, suit, value):
